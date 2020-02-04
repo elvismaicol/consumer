6b4fa74128ca271fafc60ce58ec0c0cc924849d2
elvismaicol <elvismaicol@gmail.com>
2020-02-03 21:52:42 -0300

    4ª Part - Expressão lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Program {
 
@@ -17,7 +18,12 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
         
-        list.forEach(Product :: nonStaticPriceUpdate);
+        double factor = 1.1;
+        
+        //Expressão lambda declarada
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+        
+        list.forEach(cons);
         
         list.forEach(System.out :: println);
     }
